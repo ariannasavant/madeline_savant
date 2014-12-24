f76d0b5758c5c02e2e21363d1c7e538d98c2cc7a
Arianna Savant <arianna.savant@gmail.com>
2014-12-24 13:54:57 -0800

    remove old files and replace with personal content

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,17 +1,3 @@
 get '/' do
-  # Look in app/views/index.erb
-  @cat = Category.all
-  @post = Post.all
   erb :index
-end
-
-get '/categories/:id' do
-  @current_cat = Category.find(params[:id])
-  @post = Post.where("category_id=?", params[:id])
-  erb :category
-end
-
-get '/categories/:id/post/:id' do
- @current_post = Post.find(params[:id])
- erb :post
 end
